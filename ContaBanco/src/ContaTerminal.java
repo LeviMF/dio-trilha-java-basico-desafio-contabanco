f0f46c2f255bce3a79e0aacfcddc79c20d95d1fb
Levi MF <levimferreira@outlook.com>
2023-09-15 11:46:06 -0300

    Entrega do projeto Java

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,3 +1,30 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    
+    public static void main(String[] args) throws Exception{
+        int numero;
+        String agencia;
+        String nomeCliente;
+        double saldo;
+
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da Agencia: ");
+        agencia = teclado.next();
+
+        System.out.println("Por favor, digite o número da conta: ");
+        numero = teclado.nextInt();
+        
+        //Esvazia o buffer do teclado 
+        teclado.nextLine();
+
+        System.out.println("Por favor, digite seu nome: ");
+        nomeCliente = teclado.nextLine();
+
+        System.out.println("teste:");
+        saldo = teclado.nextDouble();
+        
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque");
+    }
 }
